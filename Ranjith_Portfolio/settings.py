73f05265c2722a4fb79dda3c7487a1e248cca172
RanjithRatnala <32544850+RanjithRatnala@users.noreply.github.com>
2025-07-06 23:12:55 +0530

    Update settings.py

--- Ranjith_Portfolio/settings.py
+++ Ranjith_Portfolio/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    "ranjith-portfolio-db57.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
